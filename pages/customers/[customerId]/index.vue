c4a549c668ad30270da21c169f5054aa492fe54a
Joran Mowry <jordan.mowry@gmail.com>
2023-10-03 20:20:52 -0400

    make more responsive

--- pages/customers/[customerId]/index.vue
+++ pages/customers/[customerId]/index.vue
@@ -173,7 +173,9 @@ const paymentStatusClass = (status: string) => {
     </header>
 
     <!-- Customer details -->
-    <section class="grid grid-cols-1 bg-gray-50 sm:grid-cols-2 lg:grid-cols-4">
+    <section
+      class="grid grid-cols-1 bg-gray-50 sm:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-4"
+    >
       <div
         class="sm:border-l border-t border-gray-200 py-6 px-4 sm:px-6 lg:px-8"
       >
