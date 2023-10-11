01d3d8a007662c04a6d1d7770dc6a09238c45d94
Joran Mowry <jordan.mowry@gmail.com>
2023-10-10 20:36:33 -0400

    add funcs to utils file

--- utils.ts
+++ utils.ts
@@ -27,3 +27,36 @@ export function formatPhoneNumber(phoneNumber: string) {
 export function formatDate(dateString: string) {
   return format(new Date(dateString), 'MMMM d, yyyy');
 }
+
+export function orderStatusClass(status: string) {
+  switch (status) {
+    case 'pending':
+      return 'bg-yellow-300 text-yellow-800 px-2 py-1 rounded';
+    case 'in progress':
+      return 'bg-blue-300 text-blue-800 px-2 py-1 rounded';
+    case 'complete':
+      return 'bg-green-300 text-green-800 px-2 py-1 rounded';
+    case 'canceled':
+      return 'bg-red-300 text-red-800 px-2 py-1 rounded';
+    default:
+      return '';
+  }
+}
+
+export function paymentStatusClass(status: string) {
+  switch (status) {
+    case 'not paid':
+      return 'bg-red-200 text-red-800 px-2 py-1 rounded';
+    case 'partially paid':
+      return 'bg-yellow-200 text-yellow-800 px-2 py-1 rounded';
+    case 'fully paid':
+      return 'bg-green-200 text-green-800 px-2 py-1 rounded';
+    default:
+      return '';
+  }
+}
+
+export function stripDashesFromPhoneNumber(phoneNumber: string = '') {
+  console.log(phoneNumber);
+  return phoneNumber.replace(/-/g, '');
+}
