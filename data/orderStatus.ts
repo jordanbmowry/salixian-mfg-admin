5e66dc6c27faca120f4ed03b362c5b8b65acdb99
Joran Mowry <jordan.mowry@gmail.com>
2023-10-11 16:18:43 -0400

    dropdown data

---
+++ data/orderStatus.ts
@@ -0,0 +1,7 @@
+export const ORDER_STATUS = [
+  { name: 'Select an order status', value: '' },
+  { name: 'Pending', value: 'pending' },
+  { name: 'In progress', value: 'in progress' },
+  { name: 'Complete', value: 'complete' },
+  { name: 'Canceled', value: 'canceled' },
+];
---
+++ data/paymentStatus.ts
@@ -0,0 +1,6 @@
+export const PAYMENT_STATUS = [
+  { name: 'Select a payment status', value: '' },
+  { name: 'Not paid', value: 'not paid' },
+  { name: 'Partially paid', value: 'partially paid' },
+  { name: 'Fully paid', value: 'fully paid' },
+];
