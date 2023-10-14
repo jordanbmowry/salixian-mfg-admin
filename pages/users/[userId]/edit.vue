7762e97985cfbf931bc3e35f8177894b0d01b2ec
Joran Mowry <jordan.mowry@gmail.com>
2023-10-14 18:58:49 -0400

    hide button for non admin

--- pages/users/[userId]/edit.vue
+++ pages/users/[userId]/edit.vue
@@ -348,6 +348,7 @@ definePageMeta({
           class="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8"
         >
           <NuxtLink
+            v-if="userStore.role === 'admin'"
             to="/users"
             class="text-sm font-semibold leading-6 text-gray-900"
           >
