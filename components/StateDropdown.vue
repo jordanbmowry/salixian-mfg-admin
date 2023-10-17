8fa8dc3b0053758f726335b0743d4989cee7f338
Joran Mowry <jordan.mowry@gmail.com>
2023-10-17 17:58:35 -0400

    fixed fucked up component

--- components/StateDropdown.vue
+++ components/StateDropdown.vue
@@ -36,11 +36,11 @@
         >
           <ListboxOptions
             class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            @keydown="handleKeydown"
           >
             <input
               v-model="searchQuery"
               class="w-0 h-0 opacity-0 overflow-hidden absolute"
-              @input="onSearchInput"
               ref="hiddenInput"
             />
             <ListboxOption
@@ -149,4 +149,16 @@ watch(
   },
   { immediate: true }
 );
+
+const handleKeydown = (event: KeyboardEvent) => {
+  if (event.key === 'Enter' || event.key === 'Space') {
+    const activeOption = filteredStates.value.findIndex(
+      (state) => state === selectedState.value
+    );
+
+    if (activeOption !== -1) {
+      updateSelectedState(filteredStates.value[activeOption]);
+    }
+  }
+};
 </script>
