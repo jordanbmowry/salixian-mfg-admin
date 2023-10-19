f04264cfe82d8f3b2d204f81d09839d2f3eae05c
Joran Mowry <jordan.mowry@gmail.com>
2023-10-19 12:20:51 -0400

    aria-describedby

--- components/InputField.vue
+++ components/InputField.vue
@@ -15,6 +15,7 @@
         :type="type"
         :name="name"
         :id="id"
+        :aria-describedby="error ? id + 'Error' : undefined"
         :placeholder="placeholder"
         :autocomplete="autocomplete ? 'on' : 'off'"
         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-stone-600 sm:text-sm sm:leading-6"
